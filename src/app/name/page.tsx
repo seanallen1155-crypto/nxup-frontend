8e27b4c658c8e4f05957b87781eef5884eb8110b
Sean Allen <seanallen@seans-mbp.mynetworksettings.com>
2025-08-22 09:49:09 -0400

    Initial commit

---
+++ src/app/name/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-2xl font-bold">This is the name Screen</h1>
+    </div>
+  );
+}

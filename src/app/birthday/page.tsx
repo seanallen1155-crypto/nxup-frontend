9d6b506adb4e7490b7a6dffaf229ac7629e9b018
Sean Allen <seanallen@seans-mbp.mynetworksettings.com>
2025-08-24 09:12:14 -0400

    onboarding - added name page

--- src/app/birthday/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">This is the birthday Screen</h1>
-    </div>
-  );
-}
--- src/app/sport/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">This is the sport Screen</h1>
-    </div>
-  );
-}
